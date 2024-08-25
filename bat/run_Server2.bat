f64c36bc6e2c019cc5d2513303747d9f323b88f8
xyz8808 <xyz8808@live.cn>
2024-08-25 12:00:24 +0800

    bat files

---
+++ bat/run_Server2.bat
@@ -0,0 +1,1 @@
+start DSListDemoServer.exe ThirdPersonMap -log  -slotname=ThirdPersonMap_01 -DedicatedServerConfigPath=Z:\ue\projects\DSListDemo\WindowsServer\DedicatedServer2.config
---
+++ bat/run_Server3.bat
@@ -0,0 +1,1 @@
+start DSListDemoServer.exe Map2 -log  -slotname=ThirdPersonMap_01 -DedicatedServerConfigPath=Z:\ue\projects\DSListDemo\WindowsServer\DedicatedServer3.config
